452c1fe8747ee481b674716877dc4701d05d6607
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-04-10 00:06:34 +0530

    Update ThisKeywordDemo.java

--- src/ThisKeywordDemo.java
+++ src/ThisKeywordDemo.java
@@ -5,17 +5,34 @@ class ThisDemo {
 
     // TODO: Define setValue(int value)
     // Use 'this.value = value' to resolve naming conflict
+    void setValue(int value) {
+        this.value = value;
+    }
     
     // TODO: Define display() to print "Value: " + value
+    void display() {
+        System.out.println("Value: " + this.value);
+    }
 }
 
 public class ThisKeywordDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: Read the integer input
         
-        // TODO: Create ThisDemo object
-        // TODO: Call setValue() with the input
-        // TODO: Call display()
+        // TODO: Read the integer input
+        if (sc.hasNextInt()) {
+            int inputValue = sc.nextInt();
+            
+            // TODO: Create ThisDemo object
+            ThisDemo obj = new ThisDemo();
+            
+            // TODO: Call setValue() with the input
+            obj.setValue(inputValue);
+            
+            // TODO: Call display()
+            obj.display();
+        }
+        
+        sc.close();
     }
 }
